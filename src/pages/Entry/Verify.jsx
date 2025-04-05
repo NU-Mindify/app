2e2be83283788ed19f9e498c2236227b31a5c9e6
Francis Salles <fjsalles.04@gmail.com>
2025-04-05 11:02:45 +0800

    Animation and Music
    Added Lottie and Expo AV as animation and music libraries.
    
    + Game Music
    + Background and Loading Animation

--- src/pages/Entry/Verify.jsx
+++ src/pages/Entry/Verify.jsx
@@ -6,11 +6,16 @@ import useFirebase, {SignOut} from '../../hooks/useFirebase'
 import { useIsFocused, useNavigation } from '@react-navigation/native'
 import { getAuth } from 'firebase/auth'
 import { firebaseAuth } from '../../firebase'
+import LottieView from 'lottie-react-native'
+import plane from "../../anim/plane.json";
+import Animated, { BounceIn } from 'react-native-reanimated'
+import MindifyLogo from "../../assets/Logo.png";
 
 const Verify = () => {
   const nav = useNavigation();
 
   useEffect(() => {
+    // eslint-disable-next-line no-undef
     const a = setInterval(() => {
       console.log("Checked");
       
@@ -22,10 +27,12 @@ const Verify = () => {
           }
         });
       } else {
+        // eslint-disable-next-line no-undef
         clearInterval(a);
         return;
       }
     }, 10000);
+    // eslint-disable-next-line no-undef
     return () => clearInterval(a);
   }, [])
 
@@ -34,9 +41,42 @@ const Verify = () => {
   return (
     <AppBackground>
       <Text>Verify</Text>
+      <Animated.Image
+        entering={BounceIn}
+        source={MindifyLogo}
+        resizeMode="contain"
+        style={{ width: 400, height: 200 }}
+      />
       <View style={{ gap: 2 }}>
-        <Button onPress={sendVerifyEmail} text={"Send Verification"} />
-        <Button onPress={SignOut} text={"Sign Out"} />
+        <View
+          style={{
+            margin: 16,
+            overflow: "hidden",
+            borderColor: "black",
+            borderWidth: 1,
+            backgroundColor:'rgba(0, 0, 0, 0.4)',
+            borderRadius: 12
+          }}
+        >
+          <LottieView
+            style={{
+              width: 300,
+              height: 160,
+              marginHorizontal: 'auto',
+              // backgroundColor:'red'
+            }}
+            source={plane}
+            autoPlay
+            loop
+          />
+          <Text style={{textAlign:'center', padding:8, paddingBottom: 24, color: 'white'}}>
+            Check your email to verify your email address.
+          </Text>
+        </View>
+        <View style={{padding: 16, gap:4}}>
+          <Button onPress={sendVerifyEmail} text={"Send Verification"} />
+          <Button style={{backgroundColor: 'rgb(255, 65, 65)'}} onPress={SignOut} text={"Sign Out"} />
+        </View>
       </View>
     </AppBackground>
   );
