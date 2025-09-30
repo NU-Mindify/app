2b6242988c173c04c5c979f25d27614f52e080f7
Francis Salles <fjsalles.04@gmail.com>
2025-10-01 02:49:04 +0800

    Titles
    - Edit and Earn Titles
    - New Menu Music
    - Typing sfx on story

--- src/components/Titles.jsx
+++ src/components/Titles.jsx
@@ -11,6 +11,7 @@ import titlesList from '../data/titles.json'
 
 const Titles = ({isOpen = true, onClose = () => {}}) => {
   const { accountData, setAccountData } = useContext(AccountContext)
+  const { setToast } = useContext(ModalContext)
   const [selectedTitle, setSelectedTitle] = useState(accountData.title);
   
   const updateTitle = async (title) => {
@@ -41,20 +42,41 @@ const Titles = ({isOpen = true, onClose = () => {}}) => {
           gap: 12,
         }}
       >
+        <View style={{justifyContent:'center', alignItems:'center', gap:8}}>
         <Text
           style={{
             color: "white",
             fontFamily: "LilitaOne-Regular",
-            fontSize: 28,
+            fontSize: 32,
           }}
-        >
+          >
           Select Title
         </Text>
+        <Text
+          style={{
+            color: "white",
+            fontFamily: "LilitaOne-Regular",
+            fontSize: 16,
+          }}
+          >
+          {accountData.owned_titles.length} / {titlesList.length} titles owned
+        </Text>
+          </View>
         <ScrollView contentContainerStyle={{ gap: 8 }}>
           {titlesList &&
-            titlesList.map((titleData, index) => (
-              <TitleItem title={titleData} key={index} onPress={() => {updateTitle(titleData.title); onClose()}} />
-            ))}
+            titlesList.map((titleData, index) => {
+              if(accountData.owned_titles.includes(titleData.title)) return(
+                <TitleItem title={titleData} key={index} onPress={() => {updateTitle(titleData.title); onClose()}} />
+              )
+            }  
+          )}
+          {
+            titlesList.map((titleData, index) => {
+              if (!accountData.owned_titles.includes(titleData.title)) return (
+                <TitleItem title={titleData} key={index} onPress={() => setToast("Not Owned")} disabled={true} />
+              )
+            })
+          }
         </ScrollView>
       </Body>
     </Animated.View>
@@ -62,36 +84,23 @@ const Titles = ({isOpen = true, onClose = () => {}}) => {
   )
 }
 
-const TitleItem = ({title, isSelected = false, onPress}) => {
+const TitleItem = ({title, isSelected = false, onPress, disabled = false}) => {
   return (
     <>
       <Pressable onPress={() => isSelected ? null : onPress()} 
       style={({ pressed }) => [
-        { borderWidth:3, borderRadius:12, backgroundColor: "white" },
+        { borderWidth:3, borderRadius:12, backgroundColor: disabled ? "gray" : "white" },
       ]}>
-      <View style={{flexDirection:'row', width:250, padding:12}}>
-        <Text style={{marginEnd:'auto'}}>{title.title}</Text>
+      <View style={{flexDirection:'row', width:250, padding:12, paddingBottom:4}}>
+        <Text style={{marginEnd:'auto', fontWeight:'bold'}}>{title.title}</Text>
         {/* {isSelected &&
           <Check size={14} />
           } */}
       </View>
-      <Text style={{marginEnd:'auto'}}>{title.description}</Text>
+      <Text style={{marginEnd:'auto', paddingHorizontal:12, fontSize:12, paddingBottom:12}}>{title.description}</Text>
     </Pressable>
     </>
   )
-}
-
-export const addTitle = async (title) => {
-  const { accountData } = useContext(AccountContext);
-  const { setToast } = useContext(ModalContext);
-  if(accountData?.owned_titles.includes(title)){
-    return;
-  }
-  try {
-    const { data } = await axios.get(`${API_URL}/addTitle?user_id=${accountData._id}&title=${title}`)
-  } catch (error) {
-    setToast("Failed to add title.")
-  }
 }
 
 export default Titles
